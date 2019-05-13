c5e0a90a1ae98694795bf660d23e743d415dc34f
Tanmoy karmakar <tkarmakar27112000@gmail.com>
2019-05-13 15:19:35 +0530

    updated the pricing and the about us section

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "tkarmakar27112000@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
